88d2795fde79f731565c613498243f3f744c2a69
DiegoRmsR <Diegoramos.mt@gmail.com>
2021-02-06 15:49:42 -0500

    [Feature] function that converts a decimal number to binary for short solution

---
+++ Retos/decimalToBinary.js
@@ -0,0 +1,11 @@
+/**
+ * convert decimal number to binary number
+ *
+ * @param {number} (decimal number)
+ * 
+ * @returns {number} returns number in binary
+ */
+const decimalToBinary = num => {
+  return num.toString(2)
+}
+console.log(decimalToBinary())
