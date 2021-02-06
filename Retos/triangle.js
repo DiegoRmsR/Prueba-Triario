4b572c518be10ed5044854a828e7675b0056513e
DiegoRmsR <Diegoramos.mt@gmail.com>
2021-02-06 18:44:23 -0500

    [Feature] Method that returns the surface of the triangle

--- Retos/triangle.js
+++ Retos/triangle.js
@@ -7,6 +7,7 @@ class Triangle {
     baseline: 20,
     height: 25
   }
+
   /**
   * Method that returns the perimeter of the triangle
   */
@@ -15,4 +16,22 @@ class Triangle {
     const perimetrer = height + baseline + Math.sqrt((height * height) + (baseline * baseline));
     return (perimetrer)
   }
+
+  /**
+  * Method that returns the surface of the triangle
+  */
+  surface() {
+    const { baseline, height } = this.attributes;
+    const surface = baseline * height / 2;
+    return surface
+  }
 }
+
+const { baseline, height } = new Triangle().attributes;
+console.log(`Triangle: baseline - ${baseline},  height - ${height}`)
+
+const perimeter = new Triangle().perimeter()
+console.log("Perimeter: ", perimeter)
+
+const surface = new Triangle().surface()
+console.log("Surface: ", surface)
