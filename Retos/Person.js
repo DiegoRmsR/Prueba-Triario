a4445af984dc00ecbc4ba5ea38e6d515d58aa011
DiegoRmsR <Diegoramos.mt@gmail.com>
2021-02-07 00:01:23 -0500

    [Feature] method that obtains the RFC with name, age and sex

--- Retos/Person.js
+++ Retos/Person.js
@@ -18,6 +18,21 @@ class Person {
     const altura = this.altura;
     return (peso / altura * altura)
   }
+  /**
+  * method that obtains the RFC with name, age and sex
+  */
+  obtenerRFC() {
+    const nombre = this.nombre;
+    const edad = this.edad;
+    const sexo = this.sexo;
+    const name = nombre.toUpperCase().split(' ');
+    const RFC = name[2].substr(0, 2) + name[3].substr(0, 1) + edad + sexo
+    this.rfc = RFC
+    return RFC
+  }
 }
 const IMC = new Person("diego andres ramos ruiz", 20, "M", 68, 1.70).calcularIMC()
 console.log(IMC);
+
+const esMayorDeEdad = new Person("diego andres ramos ruiz", 20, "M", 68, 1.70).esMayorDeEdad()
+console.log(esMayorDeEdad);
